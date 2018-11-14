1ce140e349f3c51f925a5d8a61787d4a3d814a74
Shaif Bhaggoe <shaif.bhaggoe@gmail.com>
2018-11-14 09:23:29 +0100

    Added the fontawesome npm packages

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import Vuesax from 'vuesax'
 import Buefy from 'buefy'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCoffee)
+
 import 'buefy/dist/buefy.css'
 import 'vuesax/dist/vuesax.css'
 
 Vue.use(Vuesax, Buefy);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
